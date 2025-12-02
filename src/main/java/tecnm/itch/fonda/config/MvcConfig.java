4e6babb6ce4a6886daa9be0c6730f2848d9fc571
corazngr <corazong986@gmail.com>
2025-12-01 23:37:18 -0600

    Version 4

--- src/main/java/tecnm/itch/fonda/config/MvcConfig.java
+++ src/main/java/tecnm/itch/fonda/config/MvcConfig.java
@@ -1,27 +1,10 @@
 package tecnm.itch.fonda.config;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-	@Value("${file.upload-dir}")
-	private String uploadDir;
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-		Path uploadPath = Paths.get(uploadDir);
-		String uploadPathUri = uploadPath.toUri().toString();
-		System.out.println("--- Mapeando la URL /uploads/** a la carpeta física: " + uploadPathUri + " ---");
-
-		registry.addResourceHandler("/uploads/**")
-				.addResourceLocations(uploadPathUri.endsWith("/") ? uploadPathUri : uploadPathUri + "/");
-	}
+    // Ya no necesitamos mapear recursos locales a carpetas físicas.
+    // Cloudinary maneja las URLs de las imágenes automáticamente.
 }
